ffc51b808b015c1b267c1161bd8e2617ed533acf
antoinebelmont <666.strigoi@gmail.com>
2024-03-19 12:54:05 -0600

    Prueba de que funcionan las actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>ola k ase, usando dokku por que dijo toño o k ase</p>
+      <p>Prueba de que si lo configuramos correctamente</p>
     </div>
   );
 }
